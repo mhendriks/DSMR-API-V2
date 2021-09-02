9be3aad267d70784cc2de7b2c3bfc44e01983dd5
Martijn <martijn@MAC724889.fritz.box>
2021-09-02 12:20:14 +0200

    case

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define _VERSION_MAJOR 3
 #define _VERSION_MINOR 0
 #define _VERSION_PATCH 1
-#define _VERSION_BUILD 35
-#define _VERSION_DATE "21/08/2021"
-#define _VERSION_TIME "12:25:33"
+#define _VERSION_BUILD 43
+#define _VERSION_DATE "24/08/2021"
+#define _VERSION_TIME "08:10:46"
 #define _VERSION_ONLY "3.0.1"
-#define _VERSION_NOBUILD "3.0.1 (21/08/2021)"
-#define _VERSION "3.0.1+35 (21/08/2021)"
+#define _VERSION_NOBUILD "3.0.1 (24/08/2021)"
+#define _VERSION "3.0.1+43 (24/08/2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
