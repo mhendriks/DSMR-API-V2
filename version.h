f0c1bdbeb181abb08355921810f0abfa90a1a63d
Martijn Hendriks <martijn.hendriks.nl@gmail.com>
2021-09-21 22:09:00 +0200

    bin files

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define _VERSION_MAJOR 3
 #define _VERSION_MINOR 1
 #define _VERSION_PATCH 0
-#define _VERSION_BUILD 353
+#define _VERSION_BUILD 354
 #define _VERSION_DATE "19/09/2021"
-#define _VERSION_TIME "08:24:01"
+#define _VERSION_TIME "08:59:41"
 #define _VERSION_ONLY "3.1.0"
 #define _VERSION_NOBUILD "3.1.0 (19/09/2021)"
-#define _VERSION "3.1.0+353 (19/09/2021)"
+#define _VERSION "3.1.0+354 (19/09/2021)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
