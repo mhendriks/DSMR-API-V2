71a7615266d0698ab8951683a393d83af850b98d
mhendriks <m.a.hendriks@gmail.com>
2023-01-10 23:38:53 +0100

    additional settings fields

--- version.h
+++ version.h
@@ -1,11 +1,11 @@
 //The version number conforms to semver.org format
 #define _VERSION_MAJOR 3
 #define _VERSION_MINOR 5
-#define _VERSION_PATCH 3
-#define _VERSION_BUILD 1133
-#define _VERSION_DATE "12/11/2022"
-#define _VERSION_TIME "08:35:43"
-#define _VERSION_ONLY "3.5.3"
-#define _VERSION_NOBUILD "3.5.3 (12/11/2022)"
-#define _VERSION "3.5.3+1133 (12/11/2022)"
+#define _VERSION_PATCH 6
+#define _VERSION_BUILD 1159
+#define _VERSION_DATE "10/01/2023"
+#define _VERSION_TIME "00:02:59"
+#define _VERSION_ONLY "3.5.6"
+#define _VERSION_NOBUILD "3.5.6 (10/01/2023)"
+#define _VERSION "3.5.6+1159 (10/01/2023)"
 //The version information is created automatically, more information here: https://github.com/rvdbreemen/autoinc-semver
